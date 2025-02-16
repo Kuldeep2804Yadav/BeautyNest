18f74c1efc57e97f9d247d6daab1ebe99423d3f6
Kuldeep2804Yadav <kuldeep2804yadav@gmail.com>
2025-02-16 17:00:08 +0530

    fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as  Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
 import Home from "./Pages/Home";
