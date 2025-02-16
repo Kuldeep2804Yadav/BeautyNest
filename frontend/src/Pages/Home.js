949194f696af37d614d03b9961bb9a618e92542f
Kuldeep2804Yadav <kuldeep2804yadav@gmail.com>
2025-02-16 16:49:03 +0530

    remove used icons

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,8 +1,3 @@
-import React, { useRef } from "react";
-import { FaAngleLeft, FaChevronRight } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import Card from "../component/Card";
-import bannerImage from "../utils/output.jpg"; // Assuming banner image for hero section
 import HeroSection from "../component/HeroSection";
 import ProductCategory from "../component/ProductCategory";
 import ProductSlider from "../component/ProductSlider";
@@ -10,19 +5,6 @@ import ProductSlider from "../component/ProductSlider";
 const Home = () => {
  
 
-  const sliderRef = useRef(null);
-
-  const handleScroll = (direction) => {
-    const slider = sliderRef.current;
-    if (!slider) return;
-
-    const cardWidth = 320;
-    if (direction === "left") {
-      slider.scrollBy({ left: -cardWidth, behavior: "smooth" });
-    } else {
-      slider.scrollBy({ left: cardWidth, behavior: "smooth" });
-    }
-  };
 
   return (
     <div className="min-h-screen bg-[#fff] text-gray-900">
